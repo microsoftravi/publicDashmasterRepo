0d4db87767c5b8a858cbe30e583d2459545aad4d
ravicg <ravi.gupta@capgemini.com>
2025-11-13 22:19:55 +0000

    Add comments to Hello World program

--- Test.java
+++ Test.java
@@ -1,5 +1,9 @@
+/**
+ * A simple Hello World program
+ */
 public class Test {
     public static void main(String[] args) {
+        // Print Hello World to the console
         System.out.println("Hello World");
     }
 }
