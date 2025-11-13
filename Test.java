eda4dc2f4cca6d1cf932f38861123ce31d2b2a66
ravicg <ravi.gupta@capgemini.com>
2025-11-13 22:22:37 +0000

    commit changes

--- Test.java
+++ Test.java
@@ -1,7 +1,13 @@
 /**
  * A simple Hello World program
+ * This program demonstrates a basic Java application
+ * that prints a greeting message to the console.
  */
 public class Test {
+    /**
+     * Main method - entry point of the application
+     * @param args Command line arguments (not used in this program)
+     */
     public static void main(String[] args) {
         // Print Hello World to the console
         System.out.println("Hello World");
